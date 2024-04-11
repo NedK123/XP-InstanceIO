6c147802415e87dc84095d62ac22a299f6b92613
naderkahwaji <nader.kahwaji@gmail.com>
2024-04-12 00:05:03 +0300

    init

---
+++ src/main/java/org/example/xpinstanceio/XpInstanceIoApplication.java
@@ -0,0 +1,13 @@
+package org.example.xpinstanceio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpInstanceIoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XpInstanceIoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/xpinstanceio/XpInstanceIoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.xpinstanceio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XpInstanceIoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
